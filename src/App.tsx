8764c1171657807989d599afec77301a85ae6c28
arpapon <ashfaqurrahman377@gmail.com>
2024-08-27 10:31:35 +0600

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import HomePageMain from "./pages/HomePageMain";
 import MainLayout from "./PHForm/layout/MainLayout";
 import ProtectedRoute from "./PHForm/layout/ProtectedRoute";
 
--- src/PHForm/layout/MainLayout.tsx
+++ src/PHForm/layout/MainLayout.tsx
@@ -1,21 +1,14 @@
-import { Button, Layout } from "antd";
+import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
 // import { useAppDispatch } from "../../redux/feathers/hooks";
 // import { logout } from "../../redux/feathers/auth/authSlice";
 import Sidebar from "./Sidebar";
-import { useAppDispatch } from "../../redux/hooks";
-import { carUserlogout } from "../../redux/features/carAuthSlice";
+
 import NavbarUp from "../../pages/HomePage/Navbar/NavbarUp/NavbarUp";
-const { Header, Content } = Layout;
+const { Content } = Layout;
 // import { adminSidebarItems } from "../../routes/admin.routes";
 
 const MainLayout = () => {
-  const dispatch = useAppDispatch();
-
-  const handleLogout = () => {
-    dispatch(carUserlogout());
-  };
-
   return (
     <Layout style={{ height: "100vh" }}>
       <Sidebar />
